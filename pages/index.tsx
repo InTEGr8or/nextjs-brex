5e5a72a59acfd5fdd1343d2d6671ea4604f935e0
Mark Stouffer <mstouffer@westmonroe.com>
2022-11-27 14:12:47 -0800

    Add files from example

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,20 @@
-import Link from 'next/link'
+import useSWR from 'swr'
+import PersonComponent from '../components/Person'
+import { Person } from '../interfaces'
 
-export default function IndexPage() {
+const fetcher = (url: string) => fetch(url).then((res) => res.json())
+
+export default function Index() {
+  const { data, error } = useSWR('/api/people', fetcher)
+
+  if (error) return <div>Failed to load</div>
+  if (!data) return <div>Loading...</div>
+
   return (
-    <div>
-      Hello World. <Link href="/about">About</Link>
-    </div>
+    <ul>
+      {data.map((p: Person) => (
+        <PersonComponent key={p.id} person={p} />
+      ))}
+    </ul>
   )
 }
