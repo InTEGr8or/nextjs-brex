1cd0343ad870a48f7e2618058935ec16f25f3a34
Mark Stouffer <mstouffer@westmonroe.com>
2022-11-27 14:23:43 -0800

    Add files from example

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,39 @@
-import Link from 'next/link'
+import { useUser } from '@auth0/nextjs-auth0'
+import Layout from '../components/layout'
 
-export default function IndexPage() {
+const Home = () => {
+  const { user, isLoading } = useUser()
+
   return (
-    <div>
-      Hello World. <Link href="/about">About</Link>
-    </div>
+    <Layout user={user} loading={isLoading}>
+      <h1>Next.js and Auth0 Example</h1>
+
+      {isLoading && <p>Loading login info...</p>}
+
+      {!isLoading && !user && (
+        <>
+          <p>
+            To test the login click in <i>Login</i>
+          </p>
+          <p>
+            Once you have logged in you should be able to navigate between
+            protected routes: client rendered, server rendered profile pages,
+            and <i>Logout</i>
+          </p>
+        </>
+      )}
+
+      {user && (
+        <>
+          <h4>Rendered user info on the client</h4>
+          <img src={user.picture} alt="user picture" />
+          <p>nickname: {user.nickname}</p>
+          <p>name: {user.name}</p>
+        </>
+      )}
+    </Layout>
   )
 }
+
+// fast/cached SSR page
+export default Home
