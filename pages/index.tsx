2af6d292379fc862bbf6acd8155aaf9f394432fe
Mark Stouffer <mstouffer@westmonroe.com>
2022-11-28 10:01:10 -0800

    Add files from Exampls

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,41 @@
-import styles from '../styles.module.css'
+import { useEffect, useState } from 'react'
+import Loading from '../components/Loading'
 
-const Home = () => {
-  return (
-    <div className={styles.hello}>
-      <p>Hello World</p>
-    </div>
-  )
+function useMounted() {
+  const [mounted, setMounted] = useState(false)
+
+  useEffect(() => setMounted(true), [])
+
+  return mounted
 }
 
-export default Home
+export default function HomePage() {
+  const isMounted = useMounted()
+
+  return (
+    <main>
+      <section>
+        <h1>This section is server-side rendered.</h1>
+      </section>
+
+      {isMounted ? (
+        <section>
+          <h2>
+            This section is <em>only</em> client-side rendered.
+          </h2>
+        </section>
+      ) : (
+        <Loading />
+      )}
+
+      <style jsx>{`
+        section {
+          align-items: center;
+          display: flex;
+          height: 50vh;
+          justify-content: center;
+        }
+      `}</style>
+    </main>
+  )
+}
