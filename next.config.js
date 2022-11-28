3915aaa7578a3ed2f556f1393dec25ab776e5dd4
Mark Stouffer <mstouffer@westmonroe.com>
2022-11-28 09:58:39 -0800

    Add files from Exampls

--- next.config.js
+++ next.config.js
@@ -1,10 +1,19 @@
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
-})
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // any configs you need
+if (!process.env.WORDPRESS_API_URL) {
+  throw new Error(`
+    Please provide a valid WordPress instance URL.
+    Add to your environment variables WORDPRESS_API_URL.
+  `)
 }
 
-module.exports = withBundleAnalyzer(nextConfig)
+/** @type {import('next').NextConfig} */
+module.exports = {
+  images: {
+    domains: [
+      process.env.WORDPRESS_API_URL.match(/(?!(w+)\.)\w*(?:\w+\.)+\w+/)[0], // Valid WP Image domain.
+      '0.gravatar.com',
+      '1.gravatar.com',
+      '2.gravatar.com',
+      'secure.gravatar.com',
+    ],
+  },
+}
