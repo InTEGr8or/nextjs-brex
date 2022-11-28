f3df1ef6cf19e7616f1cf572ba38a42eff139202
Mark Stouffer <mstouffer@westmonroe.com>
2022-11-28 09:36:01 -0800

    Updates

--- make-branches.Notebook.ps1
+++ make-branches.Notebook.ps1
@@ -12,11 +12,11 @@ git pull
 git add .
 git commit -m "Updates"
 $examples[0..5] | ForEach-Object {
-    if(-Not $existingBranches.Contains($_.Name)) {
+    if($true -OR -Not $existingBranches.Contains($_.Name)) {
         Write-Host "Adding branch $($_.Name)"
         git checkout main
         git checkout -b $($_.Name)
-        Copy-Item $examplesPath/$($_.Name)/* .
+        Copy-Item $examplesPath/$($_.Name)/* . -Recurse -Force
         git add .
         git commit -m 'Add files from Exampls'
         git push -u origin "$($_.Name)"
