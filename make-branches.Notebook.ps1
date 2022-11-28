1c88d5bc0fe7a7b614869ea230da68b4c8bf962d
Mark Stouffer <mstouffer@westmonroe.com>
2022-11-28 09:27:38 -0800

    Add files from Exampls

--- make-branches.Notebook.ps1
+++ make-branches.Notebook.ps1
@@ -1,17 +1,23 @@
 # ## Get examples
 # Get Examples
+$projPath = "C:/repos/nextjs/nextjs-brex"
 $examplesPath = "C:/repos/nextjs/next.js/examples"
 Get-ChildItem $examplesPath | Select-Object Name | Set-Variable examples
 # Get branches
-git branch -l | ForEach-Object {$_[2..100]} | Set-Variable existingBranches
+git branch -l | ForEach-Object {$_[2..100] -Join ''} | Set-Variable existingBranches
 # ## Make branches
-$examples | ForEach-Object {
-    if($_.Name -EQ $existingBranches[0][2..100] -Join "") {
-        Write-Host "
-        $($existingBranches[0][2..100] -Join '')
+Push-Location $projPath
+git checkout main
+git pull
+$examples[0..5] | ForEach-Object {
+    if(-Not $existingBranches.Contains($_.Name)) {
+        Write-Host "Adding branch $($_.Name)"
         git checkout main
         git checkout -b $($_.Name)
-        cp $examplesPath/$($_.Name)/* .
-        "
+        Copy-Item $examplesPath/$($_.Name)/* .
+        git add .
+        git commit -m 'Add files from Exampls'
+        git push -u origin "$($_.Name)"
     }
 }
+Pop-Location 
