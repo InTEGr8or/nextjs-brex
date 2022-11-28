53d619a4321641ed24e5129562616740da051977
Mark Stouffer <mstouffer@westmonroe.com>
2022-11-28 10:10:13 -0800

    Add files from Exampls

--- next.config.js
+++ next.config.js
@@ -1,10 +1,17 @@
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
-})
+const { locales, sourceLocale } = require('./lingui.config.js')
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  // any configs you need
-}
+module.exports = {
+  i18n: {
+    locales,
+    defaultLocale: sourceLocale,
+  },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.po/,
+      use: ['@lingui/loader'],
+    })
 
-module.exports = withBundleAnalyzer(nextConfig)
+    return config
+  },
+}
