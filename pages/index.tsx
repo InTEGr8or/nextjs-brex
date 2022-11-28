8cb1d856a05768f3140a36beec204bc2f22ba960
Mark Stouffer <mstouffer@westmonroe.com>
2022-11-28 10:04:30 -0800

    Add files from Exampls

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,48 @@
-import styles from '../styles.module.css'
+import { css } from 'cssed/macro'
+import Head from 'next/head'
+import { useState } from 'react'
 
-const Home = () => {
+import { dark, light } from '../lib/theme'
+
+const styles = css`
+  .box {
+    height: 200px;
+    width: 200px;
+    margin: 0 auto;
+    margin-top: 40px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .dark {
+    background-color: ${dark};
+  }
+  .dark::before {
+    content: '🌚';
+  }
+  .light {
+    background-color: ${light};
+  }
+  .light::before {
+    content: '🌞';
+  }
+`
+
+export default function Home() {
+  const [isDark, setDark] = useState(false)
+
   return (
-    <div className={styles.hello}>
-      <p>Hello World</p>
-    </div>
+    <>
+      <Head>
+        <title>With cssed</title>
+      </Head>
+      <div
+        onClick={() => setDark((prevState) => !prevState)}
+        className={styles.box + ' ' + (isDark ? styles.dark : styles.light)}
+      >
+        Cssed demo
+      </div>
+    </>
   )
 }
-
-export default Home
