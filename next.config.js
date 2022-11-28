6f1f3ea84e30b9b1353e08793b18c101793646e3
Mark Stouffer <mstouffer@westmonroe.com>
2022-11-28 10:10:49 -0800

    Add files from Exampls

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
+const withMDX = require('@next/mdx')({
+  extension: /\.mdx?$/,
 })
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // any configs you need
-}
-
-module.exports = withBundleAnalyzer(nextConfig)
+module.exports = withMDX({
+  pageExtensions: ['js', 'jsx', 'mdx'],
+})
