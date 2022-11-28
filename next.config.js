62ae0265f2eaa461965858c066831523e22bfa06
Mark Stouffer <mstouffer@westmonroe.com>
2022-11-28 09:56:49 -0800

    Add files from Exampls

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
-})
-
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  // any configs you need
+module.exports = {
+  images: {
+    domains: [process.env.NEXT_IMAGE_DOMAIN],
+  },
 }
-
-module.exports = withBundleAnalyzer(nextConfig)
