c6afa7b4d7a5e34a0129792a64c2b599548f1a45
Mark Stouffer <mstouffer@westmonroe.com>
2022-11-28 10:16:09 -0800

    Add files from Exampls

--- next.config.js
+++ next.config.js
@@ -1,10 +1,18 @@
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
-})
-
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  // any configs you need
+module.exports = {
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      // Transform all direct `react-native` imports to `react-native-web`
+      'react-native$': 'react-native-web',
+    }
+    config.resolve.extensions = [
+      '.web.js',
+      '.web.jsx',
+      '.web.ts',
+      '.web.tsx',
+      ...config.resolve.extensions,
+    ]
+    return config
+  },
 }
-
-module.exports = withBundleAnalyzer(nextConfig)
