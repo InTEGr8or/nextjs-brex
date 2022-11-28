07c6a18abbb4969f71720f9b8a16deaaee55fbf6
Mark Stouffer <mstouffer@westmonroe.com>
2022-11-28 09:54:44 -0800

    Add files from Exampls

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,34 @@
-import useSWR from 'swr'
-import PersonComponent from '../components/Person'
-import { Person } from '../interfaces'
-
-const fetcher = (url: string) => fetch(url).then((res) => res.json())
+import { useState } from 'react'
+import styles from '../styles.module.css'
 
 export default function Index() {
-  const { data, error } = useSWR('/api/people', fetcher)
+  const [response, setResponse] = useState<Record<string, unknown> | null>(null)
 
-  if (error) return <div>Failed to load</div>
-  if (!data) return <div>Loading...</div>
+  const makeRequest = async () => {
+    const res = await fetch('/api/user')
+
+    setResponse({
+      status: res.status,
+      body: await res.json(),
+      limit: res.headers.get('X-RateLimit-Limit'),
+      remaining: res.headers.get('X-RateLimit-Remaining'),
+    })
+  }
 
   return (
-    <ul>
-      {data.map((p: Person) => (
-        <PersonComponent key={p.id} person={p} />
-      ))}
-    </ul>
+    <main className={styles.container}>
+      <h1>Next.js API Routes Rate Limiting</h1>
+      <p>
+        This example uses <code className={styles.inlineCode}>lru-cache</code>{' '}
+        to implement a simple rate limiter for API routes (Serverless
+        Functions).
+      </p>
+      <button onClick={() => makeRequest()}>Make Request</button>
+      {response && (
+        <code className={styles.code}>
+          <pre>{JSON.stringify(response, null, 2)}</pre>
+        </code>
+      )}
+    </main>
   )
 }
