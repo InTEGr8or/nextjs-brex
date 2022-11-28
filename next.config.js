03b2760c71f3871d08f79c345e71c5d12d7e28c9
Mark Stouffer <mstouffer@westmonroe.com>
2022-11-28 10:21:57 -0800

    Add files from Exampls

--- next.config.js
+++ next.config.js
@@ -1,10 +1,8 @@
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
-})
-
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  // any configs you need
+module.exports = {
+  webpack: function (config) {
+    config.externals = config.externals || {}
+    config.externals['styletron-server'] = 'styletron-server'
+    return config
+  },
 }
-
-module.exports = withBundleAnalyzer(nextConfig)
