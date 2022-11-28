85aba850212663aa073f97ff0e965943d0f77154
Mark Stouffer <mstouffer@westmonroe.com>
2022-11-28 10:24:23 -0800

    Add files from Exampls

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,22 @@
-import styles from '../styles.module.css'
+import Head from 'next/head'
+import Layout, { siteTitle } from 'components/Layout'
+import Gallery from 'components/Gallery'
+import Stats from 'components/Stats'
+import Collections from 'components/Collections'
 
 const Home = () => {
   return (
-    <div className={styles.hello}>
-      <p>Hello World</p>
-    </div>
+    <Layout>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+
+      <Stats />
+
+      <Collections />
+
+      <Gallery />
+    </Layout>
   )
 }
 
