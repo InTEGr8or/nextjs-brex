1cb4c05c5ff825a9248d20490e931dd428277c7c
Mark Stouffer <mstouffer@westmonroe.com>
2022-11-28 10:04:39 -0800

    Add files from Exampls

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,29 @@
-import styles from '../styles.module.css'
+import cxs from 'cxs'
 
-const Home = () => {
+const cx = {
+  root: cxs({
+    width: 80,
+    height: 60,
+    background: 'white',
+    ':hover': {
+      background: 'black',
+    },
+  }),
+
+  title: cxs({
+    marginLeft: 5,
+    color: 'black',
+    fontSize: 22,
+    ':hover': {
+      color: 'white',
+    },
+  }),
+}
+
+export default function Home() {
   return (
-    <div className={styles.hello}>
-      <p>Hello World</p>
+    <div className={cx.root}>
+      <h1 className={cx.title}>My page</h1>
     </div>
   )
 }
-
-export default Home
