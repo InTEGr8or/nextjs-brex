23c11b31bfa98fda1813916964582f20ce2f8660
Mark Stouffer <mstouffer@westmonroe.com>
2022-11-28 09:54:32 -0800

    Add files from Exampls

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,9 @@
-import useSWR from 'swr'
-import PersonComponent from '../components/Person'
-import { Person } from '../interfaces'
-
-const fetcher = (url: string) => fetch(url).then((res) => res.json())
-
 export default function Index() {
-  const { data, error } = useSWR('/api/people', fetcher)
-
-  if (error) return <div>Failed to load</div>
-  if (!data) return <div>Loading...</div>
-
   return (
-    <ul>
-      {data.map((p: Person) => (
-        <PersonComponent key={p.id} person={p} />
-      ))}
-    </ul>
+    <p>
+      To test the CORS route, open the console in a new tab on a different
+      domain and make a POST / GET / OPTIONS request to <b>/api/cors</b>. Using
+      a different method from those mentioned will be blocked by CORS
+    </p>
   )
 }
