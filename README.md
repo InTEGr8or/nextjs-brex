8d32ec3730746609cf389d3b140b0b59049596eb
Mark Stouffer <mstouffer@westmonroe.com>
2022-11-28 10:20:33 -0800

    Add files from Exampls

--- README.md
+++ README.md
@@ -1,27 +1,51 @@
-# Basic CSS example
+# Example app with Storybook setup for SCSS in Styled-jsx
 
-Next.js has built-in support for [CSS Modules](https://nextjs.org/docs/basic-features/built-in-css-support#adding-component-level-css) allowing you to write scoped CSS by automatically creating a unique class name. CSS Module files can be imported anywhere in your application and you don't have to worry about collisions.
+This example shows Styled-jsx (with SCSS) working for components written in TypeScript rendered both inside and outside of Storybook.
 
 ## Deploy your own
 
-Deploy the example using [Vercel](https://vercel.com?utm_source=github&utm_medium=readme&utm_campaign=next-example) or preview live with [StackBlitz](https://stackblitz.com/github/vercel/next.js/tree/canary/examples/basic-css)
+Deploy the example using [Vercel](https://vercel.com?utm_source=github&utm_medium=readme&utm_campaign=next-example) or preview live with [StackBlitz](https://stackblitz.com/github/vercel/next.js/tree/canary/examples/with-storybook-styled-jsx-scss)
 
-[![Deploy with Vercel](https://vercel.com/button)](https://vercel.com/new/git/external?repository-url=https://github.com/vercel/next.js/tree/canary/examples/basic-css&project-name=basic-css&repository-name=basic-css)
+[![Deploy with Vercel](https://vercel.com/button)](https://vercel.com/new/git/external?repository-url=https://github.com/vercel/next.js/tree/canary/examples/with-storybook-styled-jsx-scss&project-name=with-storybook-styled-jsx-scss&repository-name=with-storybook-styled-jsx-scss)
 
 ## How to use
 
 Execute [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app) with [npm](https://docs.npmjs.com/cli/init), [Yarn](https://yarnpkg.com/lang/en/docs/cli/create/), or [pnpm](https://pnpm.io) to bootstrap the example:
 
 ```bash
-npx create-next-app --example basic-css basic-css-app
+npx create-next-app --example with-storybook-styled-jsx-scss with-storybook-styled-jsx-scss-app
 ```
 
 ```bash
-yarn create next-app --example basic-css basic-css-app
+yarn create next-app --example with-storybook-styled-jsx-scss with-storybook-styled-jsx-scss-app
 ```
 
 ```bash
-pnpm create next-app --example basic-css basic-css-app
+pnpm create next-app --example with-storybook-styled-jsx-scss with-storybook-styled-jsx-scss-app
 ```
 
-Deploy it to the cloud with [Vercel](https://vercel.com/new?utm_source=github&utm_medium=readme&utm_campaign=next-example) ([Documentation](https://nextjs.org/docs/deployment)).
+### Run Storybook
+
+```bash
+npm run storybook
+# or
+yarn storybook
+```
+
+### Build Static Storybook
+
+```bash
+npm run build-storybook
+# or
+yarn build-storybook
+```
+
+You can use [Vercel](https://vercel.com/new?utm_source=github&utm_medium=readme&utm_campaign=next-example) to deploy Storybook. Specify `storybook-static` as the output directory.
+
+## Notes
+
+This example combines the following examples, with some required extra config added:
+
+- [with-storybook](https://github.com/vercel/next.js/tree/canary/examples/with-storybook)
+- [with-styled-jsx-scss](https://github.com/vercel/next.js/tree/canary/examples/with-styled-jsx-scss)
+- [with-typescript](https://github.com/vercel/next.js/tree/canary/examples/with-typescript)
--- components/header.tsx
+++ components/header.tsx
@@ -1,92 +1,89 @@
-import Link from 'next/link'
+import React from 'react'
 
-type HeaderProps = {
-  user?: any
-  loading: boolean
+import { Button } from './Button'
+
+export interface HeaderProps {
+  user?: {}
+  onLogin: () => void
+  onLogout: () => void
+  onCreateAccount: () => void
 }
 
-const Header = ({ user, loading }: HeaderProps) => {
-  return (
+export const Header: React.FC<HeaderProps> = ({
+  user,
+  onLogin,
+  onLogout,
+  onCreateAccount,
+}) => (
+  <>
     <header>
-      <nav>
-        <ul>
-          <li>
-            <Link href="/" legacyBehavior>
-              <a>Home</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/about" legacyBehavior>
-              <a>About</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/advanced/api-profile" legacyBehavior>
-              <a>API rendered profile (advanced)</a>
-            </Link>
-          </li>
-          {!loading &&
-            (user ? (
-              <>
-                <li>
-                  <Link href="/profile" legacyBehavior>
-                    <a>Client rendered profile</a>
-                  </Link>
-                </li>
-                <li>
-                  <Link href="/advanced/ssr-profile" legacyBehavior>
-                    <a>Server rendered profile (advanced)</a>
-                  </Link>
-                </li>
-                <li>
-                  <a href="/api/auth/logout">Logout</a>
-                </li>
-              </>
-            ) : (
-              <li>
-                <a href="/api/auth/login">Login</a>
-              </li>
-            ))}
-        </ul>
-      </nav>
-
-      <style jsx>{`
-        header {
-          padding: 0.2rem;
-          color: #fff;
-          background-color: #333;
-        }
-        nav {
-          max-width: 42rem;
-          margin: 1.5rem auto;
-        }
-        ul {
-          display: flex;
-          list-style: none;
-          margin-left: 0;
-          padding-left: 0;
-        }
-        li {
-          margin-right: 1rem;
-          padding-right: 2rem;
-        }
-        li:nth-child(3) {
-          margin-right: auto;
-        }
-        a {
-          color: #fff;
-          text-decoration: none;
-        }
-        button {
-          font-size: 1rem;
-          color: #fff;
-          cursor: pointer;
-          border: none;
-          background: none;
-        }
-      `}</style>
+      <div className="wrapper">
+        <div>
+          <svg
+            width="32"
+            height="32"
+            viewBox="0 0 32 32"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <g fill="none" fillRule="evenodd">
+              <path
+                d="M10 0h12a10 10 0 0110 10v12a10 10 0 01-10 10H10A10 10 0 010 22V10A10 10 0 0110 0z"
+                fill="#FFF"
+              />
+              <path
+                d="M5.3 10.6l10.4 6v11.1l-10.4-6v-11zm11.4-6.2l9.7 5.5-9.7 5.6V4.4z"
+                fill="#555AB9"
+              />
+              <path
+                d="M27.2 10.6v11.2l-10.5 6V16.5l10.5-6zM15.7 4.4v11L6 10l9.7-5.5z"
+                fill="#91BAF8"
+              />
+            </g>
+          </svg>
+          <h1>Acme</h1>
+        </div>
+        <div>
+          {user ? (
+            <Button size="small" onClick={onLogout} label="Log out" />
+          ) : (
+            <>
+              <Button size="small" onClick={onLogin} label="Log in" />
+              <Button
+                primary
+                size="small"
+                onClick={onCreateAccount}
+                label="Sign up"
+              />
+            </>
+          )}
+        </div>
+      </div>
     </header>
-  )
-}
-
-export default Header
+    <style jsx>{`
+      .wrapper {
+        font-family: 'Nunito Sans', 'Helvetica Neue', Helvetica, Arial,
+          sans-serif;
+        border-bottom: 1px solid rgba(0, 0, 0, 0.1);
+        padding: 15px 20px;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+      }
+      svg {
+        display: inline-block;
+        vertical-align: top;
+      }
+      h1 {
+        font-weight: 900;
+        font-size: 20px;
+        line-height: 1;
+        margin: 6px 0 6px 10px;
+        display: inline-block;
+        vertical-align: top;
+      }
+      button + button {
+        margin-left: 10px;
+      }
+    `}</style>
+  </>
+)
